4e007b8204ba16f74506a9df0a4374b2ff9b0b3d
unknown <pascalkemetse@gmail.com>
2020-07-29 06:16:23 -0700

    My Portfolio

--- apps.js
+++ apps.js
@@ -1,6 +1,4 @@
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const const navSlide = () => {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-links");
 
@@ -22,24 +20,6 @@ const navSlide = () => {
     //burger animation
     burger.classList.toggle("toggle");
   });
-};
-
-navSlide();
- = document.querySelector('.navlinks');
-
-    const navLink = document.querySelector('.navlinks li');
-
-    burger.addEventListener(click, () => {
-        nav.classList.toggle("nav-active");
-
-        //   burger animation 
-        burger.classList.toggle("toggle");
-        
-    });
-    navLink.forEach((link, index) => {
-        link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 3}s';
-    });
-
 };
 
 navSlide();
