fd8796a3c7ef5e50804f62982812eda60dd7af91
unknown <pascalkemetse@gmail.com>
2020-07-30 15:30:32 -0700

    My Portfolio

---
+++ jsp/app.js
@@ -0,0 +1,39 @@
+// selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+// event listeners
+todoButton.addEventListener("click", addTodo);
+
+todoList.addEventListener("click", deleteCheck);
+
+// function
+
+function addTodo(event) {
+  event.preventDefault();
+
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item");
+
+  todoDiv.appendChild(newTodo);
+
+  const completeButton = document.createElement("button");
+  completeButton.innerHTML = '<i class ="fas fa-check"></i>';
+  completeButton.classList.add("complete-btn");
+  todoDiv.appendChild(completeButton);
+
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class = " fas fa-trash"></i>';
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+
+  todoList.appendChild(todoDiv);
+  
+    todoInput.value = "";
+
+}
