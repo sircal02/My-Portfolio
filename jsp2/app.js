ee00a9984c0d3092db01ef8f075dc864757bd286
unknown <pascalkemetse@gmail.com>
2020-08-03 17:27:03 -0700

    My Portfolio

--- jsp2/app.js
+++ jsp2/app.js
@@ -1,131 +1,157 @@
-const cardscontainer = document.getElementById("cards-container");
+const cardsContainer = document.getElementById("cards-container");
 const prevBtn = document.getElementById("prev");
 const nextBtn = document.getElementById("next");
-const currentE1 = document.getElementById("current");
+const currentEl = document.getElementById("current");
 const showBtn = document.getElementById("show");
 const hideBtn = document.getElementById("hide");
-const questionE1 = document.getElementById("question");
-const answerE1 = document.getElementById("answer");
+const questionEl = document.getElementById("question");
+const answerEl = document.getElementById("answer");
 const addCardBtn = document.getElementById("add-card");
 const clearBtn = document.getElementById("clear");
 const addContainer = document.getElementById("add-container");
 
-
-// keep track of the current track
+// Keep track of current card
 let currentActiveCard = 0;
 
-//we'll be storing the dom cards in an array
-const cardsE1 = [];
+// Store DOM cards
+const cardsEl = [];
 
-//to store the data, first we have to get the data
+// Store card data
 const cardsData = getCardsData();
 
-//function to create all cards
+// const cardsData = [
+//   {
+//     question: 'What must a variable begin with?',
+//     answer: 'A letter, $ or _'
+//   },
+//   {
+//     question: 'What is a variable?',
+//     answer: 'Container for a piece of data'
+//   },
+//   {
+//     question: 'Example of Case Sensitive Variable',
+//     answer: 'thisIsAVariable'
+//   }
+// ];
+
+// Create all cards
 function createCards() {
-    cardsData.forEach((data, index) => createCards(data, index));
+  cardsData.forEach((data, index) => createCard(data, index));
 }
 
-//function to create a single card
+// Create a single card in DOM
 function createCard(data, index) {
-    const card = document.createElement('div');
-    card.classList.add('card');
-    if (index === 0) {
-        card.classList.add("active");
-    }
-    card.innerHTML = `<div class="inner-card"> 
-        <div class="inner-card-front" >
-            <p>
-            ${data.question}
-            </p>
-         </div >
-             <div>
-             <p>
-                 ${data.answer}
-             </p>
-             </div>
-    </div >`;
-    
-    card.addEventListener("click", () => card.classList.toggle("show-answer"));
-    
-    //add to dom cards
-    cardsE1.push(card);
+  const card = document.createElement("div");
+  card.classList.add("card");
 
-    cardscontainer.appendChild(card);
-    updateCurrentText();
+  if (index === 0) {
+    card.classList.add("active");
+  }
+
+  card.innerHTML = `
+  <div class="inner-card">
+  <div class="inner-card-front">
+    <p>
+      ${data.question}
+    </p>
+  </div>
+  <div class="inner-card-back">
+    <p>
+      ${data.answer}
+    </p>
+  </div>
+</div>
+  `;
+
+  card.addEventListener("click", () => card.classList.toggle("show-answer"));
+
+  // Add to DOM cards
+  cardsEl.push(card);
+
+  cardsContainer.appendChild(card);
+
+  updateCurrentText();
 }
 
-//show number of cards
+// Show number of cards
 function updateCurrentText() {
-    currentE1.innerText = `${currentActiveCard + 1}/${cardsE1.length}`;
+  currentEl.innerText = `${currentActiveCard + 1}/${cardsEl.length}`;
 }
-//fetch cards from local storage
+
+// Get cards from local storage
 function getCardsData() {
-    const cards = JSON.parse(localStorage.getItem("cards"));
-    return cards === null ? [] : cards;
+  const cards = JSON.parse(localStorage.getItem("cards"));
+  return cards === null ? [] : cards;
 }
 
-//add card to local storage
-function setCardsData() {
-    localStorage.setItem("cards", JSON.stringify(cards));
-    window.location.reload();
+// Add card to local storage
+function setCardsData(cards) {
+  localStorage.setItem("cards", JSON.stringify(cards));
+  window.location.reload();
 }
-//event listeners
-//next button
+
+createCards();
+
+// Event listeners
+
+// Next button
 nextBtn.addEventListener("click", () => {
-    cardsE1[currentActiveCard].className = "card left";
+  cardsEl[currentActiveCard].className = "card left";
 
-    currentActiveCard = currentActiveCard + 1;
+  currentActiveCard = currentActiveCard + 1;
 
-    if (currentActiveCard > cardsE1.length - 1) {
-        currentActiveCard = cardsE1.length - 1;
-    }
+  if (currentActiveCard > cardsEl.length - 1) {
+    currentActiveCard = cardsEl.length - 1;
+  }
 
-    cardsE1[currentActiveCard].className = "card active";
-    updateCurrentText();
+  cardsEl[currentActiveCard].className = "card active";
+
+  updateCurrentText();
 });
 
-//prev button
+// Prev button
 prevBtn.addEventListener("click", () => {
-    cardsE1[currentActiveCard].className = "card right";
+  cardsEl[currentActiveCard].className = "card right";
 
-    currentActiveCard = currentActiveCard - 1
-    
-    if (currentActiveCard < 0) {
-        currentActiveCard = 0;
-    }
+  currentActiveCard = currentActiveCard - 1;
 
-    cardsE1[currentActiveCard].className = "card active";
+  if (currentActiveCard < 0) {
+    currentActiveCard = 0;
+  }
 
-    updateCurrentText();
+  cardsEl[currentActiveCard].className = "card active";
+
+  updateCurrentText();
 });
 
-//show add container
+// Show add container
 showBtn.addEventListener("click", () => addContainer.classList.add("show"));
-//hide add container
+// Hide add container
 hideBtn.addEventListener("click", () => addContainer.classList.remove("show"));
-//add new cards button
+
+// Add new card
 addCardBtn.addEventListener("click", () => {
-    const question = questionE1.value;
-    const answer = answerE1.value;
+  const question = questionEl.value;
+  const answer = answerEl.value;
 
-    if (question.trim() && answer.trim()) {
-        const newCard = { question, answer };
+  if (question.trim() && answer.trim()) {
+    const newCard = { question, answer };
 
-        createCard(newCard);
+    createCard(newCard);
 
-        questionE1.value = "";
-        answerE1.value = "";
+    questionEl.value = "";
+    answerEl.value = "";
 
-        addContainer.classList.remove("show");
+    addContainer.classList.remove("show");
 
-        cardsData.push(newCard);
-        setCardsData(cardsData);
-    }
+    cardsData.push(newCard);
+    setCardsData(cardsData);
+  }
 });
 
-//clear cards button
+// Clear cards button
 clearBtn.addEventListener("click", () => {
-    localStorage.clear;
-    cardscontainer.innerHTML = "";
-    window.location.reload();
+  localStorage.clear();
+  cardsContainer.innerHTML = "";
+  window.location.reload();
 });
